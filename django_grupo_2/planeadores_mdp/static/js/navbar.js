21f3c8964a0a8b71c7a6d501d849563337e0cb04
Rafael José <rafael_jose_calderon@hotmail.com>
2023-05-02 11:17:53 -0300

    arreglos menores
    se que parece mucho codigo pero es solo humo

---
+++ django_grupo_2/planeadores_mdp/static/js/navbar.js
@@ -0,0 +1,19 @@
+function navbarColor() {
+    let scrollTop = window.scrollY;
+    let winHeight = window.innerHeight;
+
+    let base = 2 * scrollTop / winHeight;
+    let navbar = document.getElementById("navbar")
+
+    if (scrollTop < 0.5 * winHeight){
+        navbar.style.backgroundColor = `rgb(77, 93, 113, ${base})`;
+    } else {
+        navbar.style.backgroundColor = "rgb(77, 93, 113)";
+    }
+}
+
+function scrollListener() {
+    document.addEventListener("scroll", () => {navbarColor()});
+}
+
+(() => {navbarColor(); scrollListener();})();
---
+++ django_grupo_2/planeadores_mdp/templatetags/planeadores_mdp_extras.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def is_link_activate(context, url):
+    return context.request.resolver_match.url_name == url
+
+
+@register.simple_tag(takes_context=True)
+def link_activate(context, url):
+    url_name = context.request.resolver_match.url_name
+
+    if url_name == url:
+        return 'active'
