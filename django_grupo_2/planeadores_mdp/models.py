4ce08a5ed4a38165342597d060ffca95fb367104
gissestephy <gisela.s.colmeiro@gmail.com>
2023-05-05 14:57:39 -0300

    se agrego la flota aerea

--- django_grupo_2/planeadores_mdp/models.py
+++ django_grupo_2/planeadores_mdp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=20)
+
+class Aeronave(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    
