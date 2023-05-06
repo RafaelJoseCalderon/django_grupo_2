6cef5fc9aa2670edf0468275f51dc9423498596e
Rafael José <rafael_jose_calderon@hotmail.com>
2023-05-05 22:36:09 -0300

    cositas de ultimo momento

--- django_grupo_2/planeadores_mdp/static/js/form.validation.js
+++ django_grupo_2/planeadores_mdp/static/js/form.validation.js
@@ -13,3 +13,14 @@
       }, false);
     });
 })();
+
+(() => {
+  window.onload = function () {
+    var pos = window.name || 0;
+    window.scrollTo(0, pos);
+  }
+
+  window.onunload = function () {
+    window.name = self.pageYOffset || (document.documentElement.scrollTop + document.body.scrollTop);
+  }
+})();
