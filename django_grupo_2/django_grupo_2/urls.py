ce9715113c46fa621e6ca01c1988c5ca49a8fbd1
Rafael José <rafael_jose_calderon@hotmail.com>
2023-05-07 21:54:19 -0300

    retoques en aeronaves (no es el definitivo)
    * sembrando semilla para model, aunque el model ya esta, pero tuve fiaca de cambiarlo

--- django_grupo_2/django_grupo_2/urls.py
+++ django_grupo_2/django_grupo_2/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', include('planeadores_mdp.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# encontrar manera elegante para la inclusión de 'media'
+# nota: toma la primera que encuentra, mmm, un django facil.
