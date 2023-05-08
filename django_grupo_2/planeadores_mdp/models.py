9cac49369346a0baa5bbe1b4fb28290092c713d7
Rafael José <rafael_jose_calderon@hotmail.com>
2023-05-08 12:14:02 -0300

    Modelito dime tú, qué datos son los que oigo yo.

--- django_grupo_2/planeadores_mdp/models.py
+++ django_grupo_2/planeadores_mdp/models.py
@@ -2,12 +2,53 @@ from django.db import models
 
 # Create your models here.
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=20)
+# class Categoria(models.Model):
+#     nombre = models.CharField(max_length=20)
 
 class Aeronave(models.Model):
-    nombre = models.CharField(max_length=50)
-    precio = models.IntegerField()
-    descripcion = models.TextField()
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    
+    nombre = models.CharField(
+        max_length = 50,
+        verbose_name = 'Nombre'
+    )
+
+    capacidad = models.IntegerField()
+
+    motor = models.CharField(
+        max_length = 50,
+        verbose_name = 'Motor'
+    )
+
+    velocidad_crucero = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2
+    )
+
+    tanque_combustible = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2
+    )
+
+    consumo_por_hora = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2
+    )
+
+    autonomia = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2
+    )
+
+    peso_maximo = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2
+    )
+
+    equipaje = models.DecimalField(
+        max_digits = 10,
+        decimal_places = 2
+    )
+
+    imagen = models.ImageField(
+        verbose_name = 'Imagen',
+        upload_to = 'aeronaves'
+    )
--- django_grupo_2/planeadores_mdp/views.py
+++ django_grupo_2/planeadores_mdp/views.py
@@ -3,81 +3,14 @@ from django.contrib import messages
 
 from .forms import ContactoForm
 from .tools import contact_send_mail
+from .models import Aeronave
 
 def home(request):
     context = {}
     return render(request, 'home.html', context)
 
 def aeronaves(request):
-    aeronaves = (
-        {
-            'nombre': 'Piper PA-11 LV-XLT',
-            'capacidad': 2,
-            'motor': 'Continental 90',
-            'velocidad_crucero': 90,
-            'tanque_combustible': 64,
-            'consumo_por_hora': 20,
-            'autonomia': 3.2,
-            'peso_maximo': 554,
-            'equipaje': 9,
-            'imagen': 'media/aeronaves/Piper11.jpg'
-        },{
-            'nombre': 'Cessna 172 - LV-GRU',
-            'capacidad': 4,
-            'motor': 'Continental 145',
-            'velocidad_crucero': 100,
-            'tanque_combustible': 147,
-            'consumo_por_hora': 30,
-            'autonomia': 4.6,
-            'peso_maximo': 1043,
-            'equipaje': 22,
-            'imagen': 'media/aeronaves/Cessna.jpg'
-        },{
-            'nombre': 'Aero Boero 180 LV-JZY',
-            'capacidad': 4,
-            'motor': 'Lycoming 180',
-            'velocidad_crucero': 110,
-            'tanque_combustible': 64,
-            'consumo_por_hora': 20,
-            'autonomia': 3.2,
-            'peso_maximo': 8444,
-            'equipaje': 9,
-            'imagen': 'media/aeronaves/AeroBoero.jpg'
-        },{
-            'nombre': 'Piper PA-28 Cherokee LV-LMC',
-            'capacidad': 4,
-            'motor': 'Lycoming 180',
-            'velocidad_crucero': 100,
-            'tanque_combustible': 147,
-            'consumo_por_hora': 30,
-            'autonomia': 4.6,
-            'peso_maximo': 1043,
-            'equipaje': 22,
-            'imagen': 'media/aeronaves/Piper28.jpg'
-        },{
-            'nombre': 'Schleicher ASK-13 LV-EAK',
-            'capacidad': 4,
-            'motor': 'Lycoming 180',
-            'velocidad_crucero': 90,
-            'tanque_combustible': 64,
-            'consumo_por_hora': 20,
-            'autonomia': 3.2,
-            'peso_maximo': 480,
-            'equipaje': 9,
-            'imagen': 'media/aeronaves/schleicher.jpg'
-        },{
-            'nombre': 'Brasov ISD-28',
-            'capacidad': 2,
-            'motor': 'Lycoming 180',
-            'velocidad_crucero': 95,
-            'tanque_combustible': 147,
-            'consumo_por_hora': 30,
-            'autonomia': 4.6,
-            'peso_maximo': 590,
-            'equipaje': 22,
-            'imagen': 'media/aeronaves/Brasov.jpg'
-        }
-    )
+    aeronaves = Aeronave.objects.all()
 
     return render(request, 'aeronaves.html', {'aeronaves': aeronaves})
 
