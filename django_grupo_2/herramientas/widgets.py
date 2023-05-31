724d7a7ef089187bf5fe05f33d939f2914f56f13
Rafael José <rafael_jose_calderon@hotmail.com>
2023-05-31 18:39:45 -0300

    cositas

--- django_grupo_2/herramientas/widgets.py
+++ django_grupo_2/herramientas/widgets.py
@@ -3,4 +3,12 @@ from django import forms
 
 class ClearableFileInput(forms.ClearableFileInput):
     template_name = 'clearable_file_input.html'
-        
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        value = context["widget"]["value"]
+
+        if (value):
+            context["widget"]["file_name"] = str(value).split("/")[-1]
+
+        return context
