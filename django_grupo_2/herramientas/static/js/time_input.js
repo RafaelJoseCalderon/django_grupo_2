397324ea698e4a26514adf991ee15c34c84b7ca2
Rafael José <rafael_jose_calderon@hotmail.com>
2023-07-05 14:25:56 -0300

    widgets timepicker - jugando con clases js
    nota: no me gusta la "verbosidad" que a adquirido.

--- django_grupo_2/herramientas/static/js/time_input.js
+++ django_grupo_2/herramientas/static/js/time_input.js
@@ -1,12 +1,49 @@
 const hour__ = 1;
 const min__ = 5;
 
+const template = [
+    '<div class="triangle"></div>',
+    '<div class="d-flex flex-row mp--0">',
+
+        '<div class="d-flex flex-column">',
+            '<button type="button" class="btn" time-picker="up-hour">',
+                '<i class="fas fa-chevron-up"></i>',
+            '</button>',
+            '<span class="form-control time" time-picker="sp-hour"></span>',
+            '<button type="button" class="btn" time-picker="down-hour">',
+                '<i class="fas fa-chevron-down"></i>',
+            '</button>',
+        '</div>',
+
+        '<span class="d-flex align-items-center">:</span>',
+
+        '<div class="d-flex flex-column">',
+            '<button type="button" class="btn" time-picker="up-min">',
+                '<i class="fas fa-chevron-up"></i>',
+            '</button>',
+            '<span class="form-control time" time-picker="sp-min"></span>',
+            '<button type="button" class="btn" time-picker="down-min">',
+                '<i class="fas fa-chevron-down"></i>',
+            '</button>',
+        '</div>',
+
+        '<div class="d-flex flex-column justify-content-center">',
+            '<button type="button" class="btn btn-outline-secondary m-1 me-0" time-picker="now">',
+                'Ahora',
+            '</button>',
+            '<button type="button" class="btn btn-outline-secondary m-1 me-0" time-picker="close">',
+                'Cerrar',
+            '</button>',
+        '</div>',
+    '</div>',
+].join('');
+
 function showTimepicker(input) {
     let parent = input.parentElement;
     let box = document.getElementById(`${input.id}_picker`);
 
     if (!box) {
-        box = newBox(input)
+        box = (new Box(input)).render();
 
         parent.after(box);
         setTimeout(() => box.classList.add('show'), 75);
@@ -16,141 +53,99 @@ function showTimepicker(input) {
     }
 }
 
-function newBox(input) {
-    let box = document.createElement('div');
-    box.id = `${input.id}_picker`;
-    box.classList.add('timepicker-content', 'fade');
+class Box {
+    constructor(input) {
+        this.input = input
 
-    box.innerHTML = [
-        '<div class="triangle"></div>',
-        '<div class="d-flex flex-row mp--0">',
-            '<div class="d-flex flex-column" time-picker="column"></div>',
-            '<span class="d-flex align-items-center">:</span>',
-            '<div class="d-flex flex-column" time-picker="column"></div>',
-            '<div class="d-flex flex-column justify-content-center" time-picker="column"></div>',
-        '</div>'
-    ].join('');
+        this.box = document.createElement('div');
+        this.box.id = `${input.id}_picker`;
+        this.box.classList.add('timepicker-content', 'fade');
+        this.box.innerHTML = template;
 
-    let list = box.querySelectorAll('div[time-picker]');
-    let initVal = getValues(input);
-    let labelHour = newLabel(initVal.hour);
-    let lableMin = newLabel(initVal.min);
+        this.labelHour = getSpan(this.box, 'sp-hour');
+        this.lableMin = getSpan(this.box, 'sp-min');
 
-    list[0].appendChild(upHour(input, labelHour));
-    list[0].appendChild(labelHour);
-    list[0].appendChild(downHour(input, labelHour));
+        this.values = this.getValues();
+        this.setValues(this.values);
 
-    list[1].appendChild(upMin(input, lableMin));
-    list[1].appendChild(lableMin);
-    list[1].appendChild(downMin(input, lableMin));
+        setButton(this.box, 'up-hour', this.upHour.bind(this));
+        setButton(this.box, 'down-hour', this.downHour.bind(this));
 
-    list[2].appendChild(now(input, labelHour, lableMin));
-    list[2].appendChild(close(box));
-    return box;
-}
+        setButton(this.box, 'up-min', this.upMin.bind(this));
+        setButton(this.box, 'down-min', this.downMin.bind(this));
 
-function newLabel(value) {
-    let label = document.createElement('span')
-    label.classList.add('form-control', 'time');
-    label.innerHTML = ('00' + value).slice(-2);
+        setButton(this.box, 'now', this.now.bind(this));
+        setButton(this.box, 'close', this.close.bind(this));
+    }
 
-    return label;
-}
+    setValues(values) {
+        let hour_ = ('00' + values.hour).slice(-2);
+        let min_ = ('00' + values.min).slice(-2);
 
-function newButton(inner, classList, fnctn) {
-    let button = document.createElement('button');
+        this.input.value = `${hour_}:${min_}:00`;
+        this.labelHour.innerHTML = `${hour_}`;
+        this.lableMin.innerHTML = `${min_}`;
 
-    button.type = 'button';
-    button.innerHTML = inner;
-    button.addEventListener('click', fnctn);
+        this.values = values;
+    };
 
-    classList.forEach(element => {
-        button.classList.add(element);
-    });
+    getValues() {
+        let values = this.input.value.split(':');
+        return { hour: parseInt(values[0]), min: parseInt(values[1]) };
+    };
 
-    return button;
-}
-
-function getValues(input) {
-    let values = input.value.split(':');
-    return { hour: parseInt(values[0]), min: parseInt(values[1]) };
-}
-
-function upHour(input, label) {
-    return newButton('<i class="fas fa-chevron-up"></i>', ['btn'], () => {
-        let values = getValues(input);
-        setHour(input, label, {
-            hour: values.hour < 23 ? values.hour + hour__ : values.hour,
-            min: values.min
+    upHour() {
+        this.setValues({
+            hour: this.values.hour < 23 ? this.values.hour + hour__ : this.values.hour,
+            min: this.values.min
         });
-    });
-}
+    };
 
-function downHour(input, label) {
-    return newButton('<i class="fas fa-chevron-down"></i>', ['btn'], () => {
-        let values = getValues(input);
-        setHour(input, label, {
-            hour: values.hour > 0 ? values.hour - hour__ : values.hour,
-            min: values.min
+    downHour() {
+        this.setValues({
+            hour: this.values.hour > 0 ? this.values.hour - hour__ : this.values.hour,
+            min: this.values.min
         });
-    });
-}
+    };
 
-function setHour(input, label, values) {
-    let hour_ = ('00' + values.hour).slice(-2);
-    let min_ = ('00' + values.min).slice(-2);
-
-    input.value = `${hour_}:${min_}:00`;
-    label.innerHTML = `${hour_}`;
-}
-
-function upMin(input, label) {
-    return newButton('<i class="fas fa-chevron-up"></i>', ['btn'], () => {
-        let values = getValues(input);
-        setMin(input, label, {
-            hour: values.hour,
-            min: values.min < 59 ? values.min + min__ : values.min
+    upMin() {
+        let min = this.values.min < 59 ? this.values.min + min__ : this.values.min;
+        this.setValues({
+            hour: this.values.hour,
+            min: min === 60 ? 55 : min
         });
-    });
-}
+    };
 
-function downMin(input, label) {
-    return newButton('<i class="fas fa-chevron-down"></i>', ['btn'], () => {
-        let values = getValues(input);
-        setMin(input, label, {
-            hour: values.hour,
-            min: values.min > 0 ? values.min - min__ : values.min
+    downMin() {
+        this.setValues({
+            hour: this.values.hour,
+            min: this.values.min > 0 ? this.values.min - min__ : this.values.min
         });
-    });
+    };
+
+    now() {
+        let date = new Date();
+        this.setValues({
+            hour: date.getHours(),
+            min: min__ * Math.floor(date.getMinutes() / min__)
+        });
+    };
+
+    close() {
+        this.box.remove()
+    }
+
+    render() {
+        return this.box;
+    }
 }
 
-function setMin(input, label, values) {
-    let hour_ = ('00' + values.hour).slice(-2);
-    let min_ = ('00' + values.min).slice(-2);
-
-    input.value = `${hour_}:${min_}:00`;
-    label.innerHTML = `${min_}`;
+function setButton(parent, name, fnctn) {
+    parent
+        .querySelectorAll(`button[time-picker="${name}"]`)[0]
+        .addEventListener("click", fnctn);
 }
 
-function now(input, labelHour, lableMin) {
-    return newButton('Ahora', ['btn', 'btn-outline-secondary', 'm-1', 'me-0'],
-        () => {
-            let date = new Date();
-            let values = {
-                hour: date.getHours(),
-                min: min__ * Math.floor(date.getMinutes() / min__)
-            }
-            
-            setHour(input, labelHour, values);
-            setMin(input, lableMin, values);
-        }
-    );
-}
-
-function close(box) {
-    return newButton('Cerrar', ['btn', 'btn-outline-secondary', 'm-1', 'me-0'],
-        () => {
-            box.remove();
-        }
-    );
+function getSpan(parent, name) {
+    return parent.querySelectorAll(`span[time-picker="${name}"]`)[0];
 }
